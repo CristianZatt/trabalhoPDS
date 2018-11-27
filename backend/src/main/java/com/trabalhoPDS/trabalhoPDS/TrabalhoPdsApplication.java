0319ff8177f4d40de487da7253c308172c5b4475
Cristian Cesari Zatt <czatt@UNISC.BR>
2018-11-27 18:22:23 -0200

    nova tentativa atualizado

---
+++ backend/src/main/java/com/trabalhoPDS/trabalhoPDS/TrabalhoPdsApplication.java
@@ -0,0 +1,12 @@
+package com.trabalhoPDS.trabalhoPDS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabalhoPdsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabalhoPdsApplication.class, args);
+	}
+}
